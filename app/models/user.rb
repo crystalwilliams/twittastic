3732721cdefcc63a7d21f0d6486805cd45058126
Crystal Williams <cryswill10@gmail.com>
2017-10-24 18:48:00 -0500

    Merge the likes branch with the “master” branch

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,8 @@ class User < ApplicationRecord
 
   validates :name, presence: true
   validates :username, presence: true, uniqueness: true
+
+  def likes?(tweet)
+    tweet.likes.where(user_id: id).any?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   get 'profiles/:id' => 'profiles#show', as: :profile
 
   resources :relationships
-  resources :tweets
+  resources :tweets do
+    resource :likes
+  end
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
