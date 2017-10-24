5ea95562d703c126fd3638673ec9082cb74f6a12
Crystal Williams <cryswill10@gmail.com>
2017-10-23 19:06:43 -0500

    Add profile controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'profiles/index'
+
+  get 'profiles/show'
+
   resources :tweets
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
